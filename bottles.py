076aefc75d65ff0618686145fb6cc2c814398003
xTyndx <bamberg.patrick98@web.de>
2025-03-19 11:53:07 +0100

    init

---
+++ bottles.py
@@ -0,0 +1,9 @@
+# for i in range(5):
+#     print("The square of " + str(i) + " is " + str(i*i))
+
+for i in range(99, 0, -1):
+    print(f'{i} bottles of beer on the wall')
+    print(f'{i} bottles of beer')
+    print('Take one down, pass it around')
+    print(f'{i-1} bottles of beer on the wall')
+    
