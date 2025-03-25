b9da3e7f658347fc730771ecc59494ae25e2e0c9
xTyndx <bamberg.patrick98@web.de>
2025-03-25 08:55:32 +0100

    init

---
+++ Intermediate/1.py
@@ -0,0 +1,20 @@
+genz_slang_list = [
+    "Bestie",
+    "tea",
+    "cap",
+]
+
+for term in genz_slang_list:
+    print(term)
+
+
+genz_slang_dict = {
+    "besie": "best friend",
+    "tea": "gossip or news",
+
+}
+
+
+for term, definition in genz_slang_dict.items():
+    print(f"{term}: {definition}")
+
--- guess.py
+++ guess.py
@@ -3,4 +3,3 @@ while guess !=6:
     guess = int(input("Guess the number:    "))
 
 print("You got it!")
-
